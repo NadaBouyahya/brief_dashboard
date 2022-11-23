3527ca2bbb99be87b557863b8a027c347330519a
NadaBouyahya <bouyahyanada.solicode@gmail.com>
2022-11-23 16:56:55 +0100

    Update Component.js

--- react-autoformation/src/Component.js
+++ react-autoformation/src/Component.js
@@ -1,8 +1,21 @@
 export default function Test(props){
-    let myList = ["nada", "zobair", "amine"];
+    let myList = [
+        {name: "zobair", id: 1},
+        {name: "nada", id: 2},
+        {name: "amine", id: 3},
+    ];
     return <ul>
         {myList.map(function(item){
-            return <li>{item}</li>
+            return <li key={item.id}>{item.name} / {item.id}</li>
         })}
     </ul>
 }
+
+// export default function Test(props){
+//     let myList = ["nada", "zobair", "amine"];
+//     return <ul>
+//         {myList.map(function(item){
+//             return <li>{item}</li>
+//         })}
+//     </ul>
+// }
