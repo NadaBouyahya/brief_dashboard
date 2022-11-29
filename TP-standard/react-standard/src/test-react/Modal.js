3ca61c5b3233777d62f806973090a0e3258ee984
NadaBouyahya <bouyahyanada.solicode@gmail.com>
2022-11-29 19:05:39 +0100

    .

--- TP-standard/react-standard/src/test-react/Modal.js
+++ TP-standard/react-standard/src/test-react/Modal.js
@@ -1,13 +1,32 @@
+import axios from "axios";
+import React, { useEffect, useState } from "react"
+
 export default function Modal(props){
+
+    const [post, setPost] = useState([]);
+    useEffect(()=> {
+        axios.get('http://127.0.0.1:8000/api/todos/')
+        .then((response) => {
+            console.log(response.data);
+            setPost(response.data)
+        })
+    }, [])
+    
 
     if(props.show === false){
         return;
     }
 
+
     return (
         <div>
-            <h1>my modal</h1>
-            <button onClick={()=>props.closeModal(false)}>close</button>
+            <h1>edit todo</h1>
+            <form>
+                <input type="text" defaultValue={post.name}/>
+                <button onClick={()=>props.closeModal(false)}>close</button>
+
+            </form>
         </div>
     )
 }
+
--- TP-standard/react-standard/src/test-react/Test1.js
+++ TP-standard/react-standard/src/test-react/Test1.js
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from "react"
 import axios from "axios"
 import Modal from "./Modal";
+import {Link} from "react-router-dom"
 
 export default function Test1(props) {
 
