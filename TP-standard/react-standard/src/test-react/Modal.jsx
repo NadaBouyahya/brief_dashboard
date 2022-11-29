3cfb78efb0b46fa263f4d30c34054300ed4a752e
NadaBouyahya <bouyahyanada.solicode@gmail.com>
2022-11-29 19:54:54 +0100

    .

--- TP-standard/react-standard/src/test-react/Modal.jsx
+++ TP-standard/react-standard/src/test-react/Modal.jsx
@@ -19,11 +19,25 @@ export default function Modal(props){
         return;
     }
 
+    const handleChange = (e) =>{
+        const name = e.target.name;
+        const value = e.target.value;
+        setPost({...post, [name]: value});
+    }
+
+    const updateTodo = (event)=>{
+        event.preventDefault();
+        axios.put('http://127.0.0.1:8000/api/todos/' + props.show.id, post)
+    }
+
+
+
     return (
         <div>
             <h1>edit todo</h1>
-            <form>
-                <input type="text" defaultValue={post.name}/>
+            <form onSubmit={updateTodo}>
+                <input type="text" name="name" onChange={handleChange} defaultValue={post.name}/>
+                <button type="submit" >save</button>
                 <button onClick={()=>props.closeModal(false)}>close</button>
 
             </form>
